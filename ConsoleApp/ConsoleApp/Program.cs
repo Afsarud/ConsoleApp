8316dbfb2c87487f7aa17adc4a17c76ba9c33b8f
Afsarud <engafsar5577@gmail.com>
2019-09-10 16:44:09 +0600

    ConsoleApp

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name, address, contact;
+            Console.WriteLine("Enter your name please:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Age please:");
+            age = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Contact Number");
+            contact = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Address");
+            address = Console.ReadLine();
+            Console.WriteLine(name +", "+ age + ", " +contact +", "+address);
+
+            Console.ReadKey();
+        }
+    }
+}
